1ef46d86e3207a43203f2e4f849665c3177c248a
Yamatsu <sorakakeru@users.noreply.github.com>
2026-01-10 20:26:22 +0900

    作業中

---
+++ src/settings/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+config = {
+    'example.com': [
+        '/_anlz/',
+        '/_assets/',
+        '/assets/',
+        '/assets_c/',
+        '/error/',
+        '/giftbox/'
+    ],
+    'a.example.com': [
+        '/_anlz/',
+        '/_assets/'
+    ],
+    'example.net': [
+        '/_anlz/'
+    ]
+}
+
